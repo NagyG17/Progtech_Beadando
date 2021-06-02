7d484288ce2a95550cbf20d602a7e31b4113c915
NagyG17 <64412191+NagyG17@users.noreply.github.com>
2021-06-02 17:32:21 +0200

    update menük

---
+++ Items.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgtechManagment
+{
+    class Items
+    {
+        public Items(int Id,string TypeOfProduct,int Quantity)
+        {
+            this.Id = Id;
+            this.TypeOfProduct = TypeOfProduct;
+            this.Quantity = Quantity;
+        }
+        private int id;
+        public int Id
+        {
+            set
+            {
+                if (value > 0)
+                    id = value;
+            }
+            get
+            {
+                return id;
+            }
+        }
+        private string typeOfProduct;
+        public string TypeOfProduct
+        {
+            set { typeOfProduct = value; }
+            get { return typeOfProduct; }
+
+        }
+        private int quantity;
+        public int Quantity
+        {
+            set { if (value > 0)
+                    quantity = value;
+                    
+                }
+            get { return quantity; }
+        }
+        private List<Items> orders;
+        public List<Items> Orders
+        {
+            
+            get { return new List<Items>(orders); }
+        }
+
+    }
+}
---
+++ OrderMenu.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgtechManagment
+{
+    class OrderMenu : MenuBase
+    {
+        protected override void SetPrompt()
+        {
+            Prompt = "Ez az Order Management oldala. \nVálassz az alábbi lehetőségekből, add meg melyik terméket szeretnéd megrendelni!";
+        }
+        
+        private int consoleWriteClear(string productName)
+         {
+             Console.Clear();
+             Console.WriteLine("Add meg mennyi " + productName + " konzervet szeretnél rendelni!");
+             return Convert.ToInt32(Console.ReadLine());
+
+
+         }
+         
+         public Dictionary<string, int> lista = new Dictionary<string, int>();
+
+         public override void managemenu() {
+
+            char c = Console.ReadKey().KeyChar;
+            switch (c)
+                {
+                    case '1':
+                        int n = consoleWriteClear("paradicsomos");
+                        lista.Add("Paradicsomos", n);
+                        
+                        
+                        managemenu();
+
+                        break;
+
+                    case '5':
+                        Console.Clear();
+                        
+                    
+                        break;
+                    default:
+                        break;
+                }
+            
+            }
+
+
+
+        List<string> Options = new List<string>();
+        public override void SetOptions()
+        {
+            Console.Clear();
+            
+
+            Options.Add("[1] Paradicsomos konzerv");
+            Options.Add("[2] Bab konzerv");
+            Options.Add("[3] Minőségi paradicsomos konzerv");
+            Options.Add("[4] Minőségi bab konzerv");
+            Options.Add("[5] Exit");
+            SetPrompt();
+            Console.WriteLine(Prompt);
+            
+            for (int i = 0; i < Options.Count; i++)
+            {
+                Console.WriteLine(Options[i]);
+                
+            }
+
+            managemenu();
+            Options.Clear();
+
+        }
+        
+        /*public override void drawMenu()
+        {
+            Console.Clear();
+            SetPrompt();
+            Console.WriteLine(Prompt);
+            SetOptions();
+            for (int i = 0; i < Options.Count; i++)
+            {
+                Console.WriteLine(Options[i]);
+            }
+        }*/
+    }
+}
